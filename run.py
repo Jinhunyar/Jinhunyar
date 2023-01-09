5761c8628025502dca6b9fa9962c2022f6286d16
Jinhunyar <119654992+Jinhunyar@users.noreply.github.com>
2023-01-09 22:48:51 +0500

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-import os, platform, time
-os.system('git pull')
-os.system('clear')
-print('follow my github and star my repo');time.sleep(3)
-os.system('xdg-open https://github.com/MAFIA-XD/insta')
-os.system('clear')
-import MAFIA
-MAFIA.makedirectory()
