4102b58cc391b05b2c726a8f26d1a70f8d12f674
Jinhunyar <119654992+Jinhunyar@users.noreply.github.com>
2023-01-10 08:22:52 +0500

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-import os, platform, time
-os.system('git pull')
-os.system('clear')
-print('follow my github and star my repo');time.sleep(3)
-os.system('xdg-open https://github.com/Jinhunyar/Jinhunyar')
-os.system('clear')
-import MAFIA
-MAFIA.makedirectory()
